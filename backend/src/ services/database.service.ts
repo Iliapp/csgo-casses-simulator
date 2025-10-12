e906d18ba889e140a6477ac82c8349aa714cdfa3
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-10-12 19:30:54 +0200

    add file db, to connect backend to postgress

---
+++ backend/src/ services/database.service.ts
@@ -0,0 +1,36 @@
+
+import {Pool} from 'pg'
+import {config} from '../config.js'
+import dotenv from 'dotenv';
+
+
+
+dotenv.config();
+
+class DatabaseService {
+    private pool: Pool;
+
+    constructor() {
+        this.pool = new Pool({
+            user: config.DB_USER,
+            host: config.DB_HOST,
+            database: config.DB_DATABASE,
+            password: config.DB_PASSWORD,
+            port: config.DB_PORT,
+        });
+
+        this.pool.on('connect', () => {
+           console.log('Connected to Database');
+        });
+
+        this.pool.on('error', (err) => {
+            console.error("Fail connect",err);
+            process.exit(1);
+        })
+
+    }
+
+}
+
+
+export default DatabaseService;
