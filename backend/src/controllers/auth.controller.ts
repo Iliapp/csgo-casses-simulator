314b076b718af906c0f0fbb86a07b6e78aaa09c5
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-11-09 16:32:32 +0100

    add/ resetPassword in auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import {Request,Response} from "express";
+import {Request, response, Response} from "express";
 // import {UserModel} from "../models/user.model.js";
 import AuthService from "../ services/auth.service.js";
 
@@ -37,6 +37,17 @@ export class AuthController {
             response.status(401).json({ message: error.message });
         }
     }
+
+    async resetPassword(request: Request, response: Response) {
+        try {
+            const { email, password } = request.body;
+            const result = await authService.resetPassword(email, password);
+            response.status(200).json({ message: result });
+        } catch (error: any) {
+            response.status(401).json({ message: error.message });
+        }
+    }
+
 
 
 
