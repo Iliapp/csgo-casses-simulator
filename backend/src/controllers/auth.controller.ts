920d1d2be43db6f4af1510886c605192ca5d7bfa
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-11-09 15:50:49 +0100

    add/ log-in in auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -5,14 +5,25 @@ import AuthService from "../ services/auth.service.js";
 const authService = new AuthService();
 
 export class AuthController {
-
+// register
     async signUp(request: Request, response: Response) {
         try {
             const {email, password, name} = request.body;
             const result = await authService.signUp(email, password, name);
             response.status(201).json(result);
-        } catch (error) {
-            response.status(400).json(error);
+        } catch (error: any) {
+            response.status(400).json(error.message);
+        }
+    }
+
+    // log in
+    async singIn(request: Request, response: Response) {
+        try {
+            const {email, password} = request.body;
+            const result = await authService.signIn(email, password);
+            response.status(200).json(result);
+        } catch (error: any) {
+            response.status(400).json(error.message);
         }
     }
 
