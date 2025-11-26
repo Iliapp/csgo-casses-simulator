9084547bd61cc970aca4ca03c7ff9b19fa00e802
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-11-26 21:18:46 +0100

    add UserController and constructor

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,1 +1,12 @@
-// In this file i must add SOONER
+import {UserService} from "../ services/user.service.js";
+import { Request, Response } from "express";
+
+
+
+export class UserController {
+    private userService: UserService;
+    constructor(userService : UserService) {
+        this.userService = userService
+    }
+
+}
