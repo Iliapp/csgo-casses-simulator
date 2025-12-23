52729804f20a2937dbd9ed156758704af2cb8a1c
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-12-23 18:51:09 +0200

    feature/add/roter/delete

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import {Router} from "express";
 import {UserController} from "../controllers/users.controller.js";
+import { UserService } from "../ services/user.service.js"
+import DatabaseService from "../ services/database.service.js"
 import { Request, Response } from 'express';
 
+
 const router = Router();
+
+const databaseService = new DatabaseService();
+const userService = new UserService(databaseService);
 const userController = new UserController(userService);
 
 
@@ -13,4 +19,7 @@ router.put("/users/:email/password", (req: Request, res: Response) => userContro
 router.put("/users/:email/displayName", (req: Request, res: Response) => userController.updateUserDisplayName(req,res))
 router.put("/users/:email/balance", (req: Request, res: Response) => userController.updateUserBalance(req,res))
 router.put("/users/:email/role", (req: Request, res: Response) => userController.updateUserRole(req,res))
+router.delete("/users/:email", (req: Request, res: Response) => userController.deleteUser(req,res))
 
+
+export default router;
